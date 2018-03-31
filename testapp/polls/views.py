6f21806421fc0f0a99ab6c82af215ed23e833fb4
hmnshu <hmnshu2013@live.com>
2018-03-31 17:13:05 -0500

    New updates
    Changed recently

--- testapp/polls/views.py
+++ testapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+
 from .models import Choice, Question
 
 
@@ -22,7 +23,6 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
 	
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""testapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from signup import views as signup_views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+	path('', signup_views.home, name='home'),
+	path('login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
+	path('logout/', auth_views.logout, {'next_page': 'login'}, name='logout'),
+	path('signup/', signup_views.signup, name='signup'),
 ]
